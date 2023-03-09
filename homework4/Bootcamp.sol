fe402b01f9ab7ddd662cdc423ebb68a6c6e2410e
longnguyen1802 <96562911+longnguyen1802@users.noreply.github.com>
2023-03-10 04:25:16 +0800

    Create Bootcamp.sol

---
+++ homework4/Bootcamp.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: None
+
+pragma solidity 0.8.17;
+
+
+contract BootcampContract {
+
+    uint256 number;
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+
+    function getAddress() external view returns (address){
+        if(msg.sender==owner){
+            return address(0x000000000000000000000000000000000000dEaD);
+        }
+        return owner;
+    }
+}
